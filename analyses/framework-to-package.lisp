bdca224a0b2ecdde1a9567dc3ee040fb083c7319
hes <hes@csail.mit.edu>
2017-02-08 13:25:24 -0500

    Initial setup

---
+++ analyses/framework-to-package.lisp
@@ -0,0 +1,7 @@
+;;; -*- Mode: Common-lisp; Package: Natsoft; readtable: Joshua -*-
+
+(jmonkeyengine "com.jme3.scene.shape" 3d-shape)
+(jreactphysics3d "net.smert.jreactphysics3d.collision.shapes" 3d-shape)
+(react "com.flowpowered.react.collision.shape" 3d-shape)
+(dyn4j "org.dyn4j.geometry" 2d-shape)
+(jbox2d "org.jbox2d.collision.shapes" 2d-shape)
